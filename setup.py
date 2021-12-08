f176a831c4209055b842f5d9b56c57eed3bf5138
Pete Jones <peterjones252@gmail.com>
2021-12-08 17:21:52 +0000

    Add command

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
+import argparse
 from setuptools import setup, find_packages
 setup(
     name="sagittal_average",
     version="0.1.0",
     packages=find_packages(),
+    install_requires=['numpy','argparse'],
+    entry_points ={
+        'console_scripts': [
+            'sagittal_average_run = command:run'
+        ]
+    }
 )
